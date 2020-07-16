b2d7f0805fdb5b8f806b2a7e783f7766cc92287a
TabataAkihiro <67178341+TabataAkihiro@users.noreply.github.com>
2020-07-16 11:23:23 +0900

    背景差し替え用jsスクリプト．

---
+++ pbl-10_im_change.js
@@ -0,0 +1,6 @@
+window.onload = PblSetBg;
+function PblSetBg() {
+    document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/PBL-10/img/master/usa_bg_1.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+}
